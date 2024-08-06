bcf85b3b7530f4f0f0946cd69a98707e28a6264d
NitsugaSamot <tomaslona888@gmail.com>
2024-08-06 02:07:43 -0600

    chnge route on loginForm

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,30 +1,37 @@
-import React from 'react';
+'use client'
 import { Layout, Menu, Typography } from 'antd';
 import Image from 'next/image';
 import Link from 'next/link';
+
 
 const { Header: AntHeader } = Layout;
 const { Title } = Typography;
 
 const Header: React.FC = () => {
+
   return (
-    <AntHeader style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0 20px' }}>
+    <AntHeader style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0 20px', backgroundColor: '#001529' }}>
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <Image src="/images/QVitaeLogo.png" alt="QVitae Logo" width={50} height={50} />
-        <Title level={3} style={{ color: 'white', margin: '0 20px' }}>QVitae</Title>
+        <Title level={3} style={{ color: 'white', margin: '0 20px', lineHeight: '50px' }}>QVitae</Title>
       </div>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['test']}>
-        <Menu.Item key="test">
+      <Menu 
+        theme="dark" 
+        mode="horizontal" 
+
+        style={{ lineHeight: '50px', backgroundColor: 'transparent' }}
+      >
+        <Menu.Item key="/introTest">
           <Link href="/introTest">
             Test
           </Link>
         </Menu.Item>
-        <Menu.Item key="adminpanel">
+        <Menu.Item key="/adminpanel">
           <Link href="/adminpanel">
             Admin Panel
           </Link>
         </Menu.Item>
-        <Menu.Item key="logout">
+        <Menu.Item>
           Logout
         </Menu.Item>
       </Menu>
